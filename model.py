acc7ed9db66345255931265a94c086b6a0f3634a
Jens Christian Hillerup <jens@hillerup.net>
2018-01-25 15:32:54 +0100

    it can do something

--- model.py
+++ model.py
@@ -25,9 +25,66 @@ class ShiftSlot:
 
 
 class Volunteer:
-
     name = None
     can_drive = False
     has_car = False
     phone_number = None
+
+    def resolve_driver(self, i):
+        if i == "Ja": return 1
+        if i == "Nej": return 0
+        if i == "Jeg har kørekort men er helst fri for at køre": return 0.5
+
+    def resolve_has_car(self, i):
+        if i.lower() == "ja": return 1
+
+        return 0
+
+    def resolve_times(self, i):
+        times = i.split(", ")
+        out = []
+
+        if "Opsætningsvagt fredag d. 23/3 til lørdag d. 24/3: Til opsætning/forberedelse af stævnet fra kl. 15 fredag til 15 lørdag dag (Selvfølgelig med søvn og pauser indregnet!)" in times: out.append(0)
+        if "Lørdag d. 24/3" in times: out.append(1)
+        if "Søndag d. 25/3" in times: out.append(2)
+        if "Mandag d. 26/3" in times: out.append(3)
+        if "Tirsdag d. 27/3" in times: out.append(4)
+        if "Onsdag d. 28/3" in times: out.append(5)
+        if "Torsdag d. 29/3" in times: out.append(6)
+        if "Fredag d. 30/3" in times: out.append(7)
+        if "Nedtagningsvagt fredag d. 30/3 til lørdag d. 31/3: Nedtagning af ROD fra fredag eftermiddag til lørdag ved middagstid. (Der er mulighed for at aftale afgangstidspunkt lørdag med arbejdsgruppen)." in times: out.append(8)
     
+        return(out)
+
+    # 17,18,19
+    def resolve_driver_choices(self, i):
+        if i == "Det er tiptop": return 4
+        if i == "Det har jeg det okay med": return 3
+        if i == "Det vil jeg helst undgå": return 2
+        if i == "Det har jeg det skidt med": return 1
+        if i == "Jeg har ikke kørekort": return 0
+
+        return -1
+
+    # 20,21,22,23
+    def resolve_shift_type_choices(self, i):
+        if i == "Det synes jeg er mega fedt!": return 3
+        if i == "Det har jeg det fint med": return 2
+        if i == "Det vil jeg helst ikke": return 1
+        if i == "Det vil jeg meget gerne undgå": return 0
+        return -1    
+
+
+    def __init__(self, gdocs_line):
+        self.name = gdocs_line[1]
+        driver = self.resolve_driver(gdocs_line[8])
+        has_car = self.resolve_has_car(gdocs_line[9])
+    
+        sober_day = self.resolve_driver_choices(gdocs_line[17])
+        sober_sleeping_night = self.resolve_driver_choices(gdocs_line[18])
+        sober_wake_night = self.resolve_driver_choices(gdocs_line[19])
+        
+        late_night = self.resolve_shift_type_choices(gdocs_line[20])
+        kitchen = self.resolve_shift_type_choices(gdocs_line[21])
+        bar = self.resolve_shift_type_choices(gdocs_line[22])
+        high_tempo = self.resolve_shift_type_choices(gdocs_line[23])
--- test.py
+++ test.py
@@ -1,20 +1,30 @@
+from loader import get_shifts_from_csv, get_volunteers_from_csv
 from ortools.constraint_solver import pywrapcp
 
+# hver vagt er en IntVar, og dens værdi er hvilken frivillig der tager den
 solver = pywrapcp.Solver('rod')
 
-num_slots = 5
-num_volunteers = 10
 
-# hver vagt er en IntVar, og dens værdi er hvilken frivillig der tager den
+volunteers = get_volunteers_from_csv()
+shifts = get_shifts_from_csv()
 slots = []
 
-for i in range(num_slots):
+# A shift has a slot per person needed
+for shift in shifts:
+    # Make a slot for each person needed for a shift
+
+    local_slots = []
+    for i in range(shift.num_people):
+        local_slots.append(solver.IntVar(0, len(volunteers)-1, str(shift)))
+
+    # make sure that we're not assigning the same people to two slots on the same shift
+    solver.Add(solver.AllDifferent(local_slots))
+    slots.extend(local_slots)
+
     # når vi laver slots udfra en spec over stævnet, så tilføj constraints
     # med det samme så vi ikke får de samme hjælpere i samtidige slots
-
     # evt. AllDifferent for slots der overlapper i tid
-    
-    slots.append(solver.IntVar(0, num_volunteers, "Vagt %d" % i))
+
 
 # ### Hard constraints:
 # Ikke to steder samtidig
@@ -30,10 +40,10 @@ for i in range(num_slots):
 # En fridag til hver frivillig
 # Tag højde for ønsker ifht. arbejdstid og arbejdstype
 
-solver.Add(slots[0] < slots[1])
-solver.Add(slots[1] < slots[2])
-solver.Add(slots[2] < slots[3])
-solver.Add(slots[3] < slots[4])
+#solver.Add(slots[0] < slots[1])
+#solver.Add(slots[1] < slots[2])
+#solver.Add(slots[2] < slots[3])
+#solver.Add(slots[3] < slots[4])
 
 # decision builder
 db = solver.Phase(slots, solver.CHOOSE_FIRST_UNBOUND, solver.ASSIGN_MIN_VALUE)
@@ -44,6 +54,8 @@ count = 0
 while solver.NextSolution():
     count += 1
     print("Solution", count, '\n')
-    print(slots)
+
+    for slot in slots:
+        print ("%s: %s" % (slot, volunteers[slot.Value()].name))
     
 print("Number of solutions:", count)
